7399cf0be7dedcadc5322de3858c4884f67bd4bb
Flaboere <flaboere@gmail.com>
2014-02-20 15:36:48 +0100

    Øvelser t.o.m. Move 5A

---
+++ Assets/Scripts/Øvelser/MoveØvelse5A.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveØvelse5A : MonoBehaviour 
+{
+	public Transform otherObject;
+	public float speedFactor = 0.2f;
+	public Vector3 speed = Vector3.zero;
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 toOtherObject = otherObject.transform.position - transform.position;
+
+		if (toOtherObject.magnitude > 1f) 
+		{
+			toOtherObject.Normalize ();
+
+			speed = toOtherObject * speedFactor;
+
+			transform.position = transform.position + speed * Time.deltaTime;
+		}
+	}
+}
---
+++ Assets/Scripts/Øvelser/RotateØvelse1A.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateØvelse1A : MonoBehaviour 
+{
+	public float speed = 45f;
+	public Vector3 direction;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Rotate (direction, speed * Time.deltaTime);
+	}
+}
