1a4f41f17c69b07360d147acd398a25b1bd785d6
Flaboere <flaboere@gmail.com>
2014-03-12 13:57:20 +0100

    mores tff

---
+++ Assets/Scripts/PlayerAlter.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAlter : MonoBehaviour 
+{
+	public CharacterMotor charMotor;
+	public PlayerMovement playerMovement;
+	public float extraSpeedSideways = 50f;
+	public bool extraSpeed = true;
+	public bool giveJump = false;
+	// Use this for initialization
+	void Start () 
+	{
+		charMotor = GameObject.FindObjectOfType<CharacterMotor> ();
+		playerMovement = GameObject.FindObjectOfType<PlayerMovement> ();
+	}
+	void OnTriggerEnter(Collider hit)
+	{
+		if (hit.collider.CompareTag ("Player")) 
+		{
+			if (extraSpeed)
+			{
+			charMotor.movement.maxSidewaysSpeed += extraSpeedSideways;
+			}
+			if (giveJump = true)
+			{
+				playerMovement.canDoubleJump = true;
+			}
+		}
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+}
---
+++ Assets/Scripts/VertexColor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VertexColor : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+		Vector3[] vertices = mesh.vertices;
+		Color[] colors = new Color[vertices.Length];
+		int i = 0;
+		while (i < vertices.Length) {
+			colors[i] = Color.Lerp(Color.red, Color.green, vertices[i].y);
+			i++;
+		}
+		mesh.colors = colors;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
